31a80c8edb4e60201f439f0117dcba4b21898544
Kim Toivonen <kim.toivonen@outlook.com>
2018-09-10 13:51:51 +0300

    Created a mocktest as well as a travis.yml file

---
+++ tests/testscript.py
@@ -0,0 +1,17 @@
+import unittest
+
+__name__ = "main"
+
+def f(x):
+    return 30000 + (100 * x)
+
+
+
+class Mocktest(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(31500, f(15))
+
+
+if __name__ == '__main__':
+    unittest.main()

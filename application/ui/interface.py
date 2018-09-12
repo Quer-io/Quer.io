aa4e8d3789780e2f965b185b7f43d144bdd1586d
frestmau <mauri.frestadius@hotmail.fi>
2018-09-12 11:51:04 +0300

    Test script can run modules from application.

--- application/ui/interface.py
+++ application/ui/interface.py
@@ -2,9 +2,14 @@ import ml
 
 def baseUi():
     print("Welcome to Quer.io")
+    displayExample()
     x = int(input("Give age to search: "))
     displayTest(x)
 
 def displayTest(x):
     x = ml.predict_income(x)
     print("Expected value is " + repr(x))
+
+def displayExample():
+    print("The database contains data in this form:")
+    print(ml.get_example_row_from_db())
--- tests/testscript.py
+++ tests/testscript.py
@@ -1,4 +1,7 @@
 import unittest
+import sys
+sys.path.append('../')
+import application
 
 __name__ = "main"
 
@@ -11,6 +14,9 @@ class Mocktest(unittest.TestCase):
 
     def test(self):
         self.assertEqual(31500, f(15))
+
+    def testResult(self):
+        self.assertEqual(application.ml.model.predict_income(1), 10.5)
 
 
 if __name__ == '__main__':

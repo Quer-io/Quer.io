b8a89d124c7e3174cfe51fb82e361333e7936499
JaykobJ <43149056+JaykobJ@users.noreply.github.com>
2018-09-14 17:49:10 +0300

    ml component uses now SQLAlchemy and connects to Heroku DB

--- application/ml/model.py
+++ application/ml/model.py
@@ -1,3 +1,8 @@
+from sqlalchemy import create_engine
+
+db_string = "postgres://otoihucuckhivv:7b93b9777ab13649dc0af7ef499a699a307c7ffd5ca1733389e1dfb1dac5253a@ec2-54-217-250-0.eu-west-1.compute.amazonaws.com:5432/dab0467utv53cp"
+
+db = create_engine(db_string)
 
 def predict_income(age):
     return age * 10.5

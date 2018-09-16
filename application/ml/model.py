f2edbd1a5cf6e9edc5485a0241e11387638763ce
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-09-16 17:25:02 +0300

    Actually return a dictionary from ml.get_example_row_from_db

--- application/ml/model.py
+++ application/ml/model.py
@@ -1,11 +1,13 @@
-from sqlalchemy import create_engine
+import sqlalchemy
 
 db_string = "postgres://otoihucuckhivv:7b93b9777ab13649dc0af7ef499a699a307c7ffd5ca1733389e1dfb1dac5253a@ec2-54-217-250-0.eu-west-1.compute.amazonaws.com:5432/dab0467utv53cp"
 
 # represents the core interface to the database
-engine = create_engine(db_string)
-
+engine = sqlalchemy.create_engine(db_string)
 conn = engine.connect()
+md = sqlalchemy.MetaData()
+table = sqlalchemy.Table('person', md, autoload_with=engine)
+
 
 def predict_income(age):
     return age * 10.5
@@ -18,7 +20,6 @@ def get_example_row_from_db():
     the values are the values of the table corresponding to the columns.
     """
 
+    column_names = table.columns.keys()
     result = conn.execute("SELECT * FROM person").fetchone()
-
-    return result
-    # return {'age': 30, 'income': predict_income(30)}
+    return {column_name: result[column_name] for column_name in column_names}

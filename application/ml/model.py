719aa46799282ac56fc15baf995334144c3f7606
JaykobJ <43149056+JaykobJ@users.noreply.github.com>
2018-09-16 11:21:33 +0300

    Fixed sqlalchemy to fetch first row from Heroku db

--- application/ml/model.py
+++ application/ml/model.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 
 db_string = "postgres://otoihucuckhivv:7b93b9777ab13649dc0af7ef499a699a307c7ffd5ca1733389e1dfb1dac5253a@ec2-54-217-250-0.eu-west-1.compute.amazonaws.com:5432/dab0467utv53cp"
 
-db = create_engine(db_string)
+# represents the core interface to the database
+engine = create_engine(db_string)
 
+conn = engine.connect()
 
 def predict_income(age):
     return age * 10.5
@@ -15,4 +17,8 @@ def get_example_row_from_db():
     The keys of the dictionary are the column names of the database table,
     the values are the values of the table corresponding to the columns.
     """
-    return {'age': 30, 'income': predict_income(30)}
+
+    result = conn.execute("SELECT * FROM person").fetchone()
+
+    return result
+    # return {'age': 30, 'income': predict_income(30)}

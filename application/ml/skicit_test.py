3fb53d0b98b69d25d6741a317f7c153514dccd91
xbexbex <xbexbex@github.com>
2018-09-23 18:47:59 +0100

    change classifier to regressor

--- application/ml/skicit_test.py
+++ application/ml/skicit_test.py
@@ -3,9 +3,9 @@ import numpy as np
 import sqlalchemy
 
 from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_absolute_error
 
 db_string = "postgres://otoihucuckhivv:7b93b9777ab13649dc0af7ef499a699a307c7ffd5ca1733389e1dfb1dac5253a@ec2-54-217-250-0.eu-west-1.compute.amazonaws.com:5432/dab0467utv53cp"
 engine = sqlalchemy.create_engine(db_string)
@@ -13,7 +13,7 @@ engine = sqlalchemy.create_engine(db_string)
 data = pd.read_sql('SELECT * FROM person', engine)
 
 train, test = train_test_split(data, test_size = 0.15)
-c = DecisionTreeClassifier(min_samples_split=10)
+c = DecisionTreeRegressor()
 
 #to-do: add profession
 features = ["age", "income", "height"]
@@ -27,9 +27,9 @@ y_test = train["github_stars"]
 dt = c.fit(X_train, y_train)
 
 y_pred = c.predict(X_test)
-score = accuracy_score(y_test, y_pred) * 100
+mae = mean_absolute_error(y_test, y_pred)
 
-#print("Accuracy of estimating Github stars: ", round(score, 1), "%")
+#print("Mean absolute error: ", mae)
 
-def return_accuracy():
-    return [features, round(score, 1)]
+def return_mean_absolute_error():
+    return [features, mae]

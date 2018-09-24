9d3d82cadb456260d7f673096ee89a5388f1daaf
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-09-24 14:33:34 +0300

    Add Model class

--- application/ml/model.py
+++ application/ml/model.py
@@ -1,1 +1,26 @@
-# ONKO TURHA?????
+import sklearn
+import sklearn.tree
+import sklearn.model_selection
+
+# Should be removed when data is fetched from db module
+import sqlalchemy
+import pandas as pd
+
+
+class Model:
+    def __init__(self, db_string):
+        self.tree = sklearn.tree.DecisionTreeRegressor(criterion='mse')
+        engine = sqlalchemy.create_engine(db_string)
+        data = pd.read_sql('SELECT * FROM person', engine)
+        train, test = sklearn.model_selection.train_test_split(data)
+        self.tree.fit(train['age'], train['income'])
+        self.accuracy_score = tree.score(test['age'], test['income'])
+
+    def predict(self, age):
+        """Returns a tuple with (mean, variance)"""
+        node_index = self.tree.apply([age])[0]
+        return (tree.tree_.value[node_index], tree.tree_.impurity[node_index])
+
+    def get_accuracy_score(self):
+        """Returns the R^2 accuracy score of the ML model on the test data."""
+        return self.accuracy_score

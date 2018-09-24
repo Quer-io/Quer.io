a357d2c703906e5b041dc75871f99c420296bc7a
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-09-24 15:05:32 +0300

    Correct errors and output of Model

--- application/ml/model.py
+++ application/ml/model.py
@@ -9,17 +9,25 @@ import pandas as pd
 
 class Model:
     def __init__(self, db_string):
-        self.tree = sklearn.tree.DecisionTreeRegressor(criterion='mse')
+        self.tree = sklearn.tree.DecisionTreeRegressor(
+            criterion='mse',
+            random_state=42
+        )
         engine = sqlalchemy.create_engine(db_string)
         data = pd.read_sql('SELECT * FROM person', engine)
-        train, test = sklearn.model_selection.train_test_split(data)
-        self.tree.fit(train['age'], train['income'])
-        self.accuracy_score = tree.score(test['age'], test['income'])
+        train, test = sklearn.model_selection.train_test_split(
+            data, random_state=42
+        )
+        self.tree.fit(train[['age']], train['income'])
+        self.accuracy_score = self.tree.score(test[['age']], test['income'])
 
     def predict(self, age):
         """Returns a tuple with (mean, variance)"""
-        node_index = self.tree.apply([age])[0]
-        return (tree.tree_.value[node_index], tree.tree_.impurity[node_index])
+        node_index = self.tree.apply([[age]])[0]
+        return (
+            self.tree.tree_.value[node_index][0][0],
+            self.tree.tree_.impurity[node_index]
+        )
 
     def get_accuracy_score(self):
         """Returns the R^2 accuracy score of the ML model on the test data."""

545cddaa7984acab006ec71125d65115fbd3fb8b
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-09-24 15:11:02 +0300

    Use Model class in default_service

--- application/service/default_service.py
+++ application/service/default_service.py
@@ -2,11 +2,14 @@ import ml
 import db
 
 
+db_string = "postgres://otoihucuckhivv:7b93b9777ab13649dc0af7ef499a699a307c7ffd5ca1733389e1dfb1dac5253a@ec2-54-217-250-0.eu-west-1.compute.amazonaws.com:5432/dab0467utv53cp"
+model = ml.Model(db_string)
+
 def get_example_from_db():
     return db.get_example_row_from_db()
 
 def accuracy():
-    return ml.return_accuracy()
+    return ['age', model.get_accuracy_score()]
 
 def get_avg_single_param(param):
     return db.get_avg_one_param(param)

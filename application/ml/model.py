7753bf8916ce41694a13a98fe490dba57a3baaeb
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-09-28 14:16:01 +0300

    Make Model support other features and outputs

--- application/ml/model.py
+++ application/ml/model.py
@@ -2,33 +2,39 @@ import sklearn
 import sklearn.tree
 import sklearn.model_selection
 
-# Should be removed when data is fetched from db module
-import sqlalchemy
-import pandas as pd
-
 
 class Model:
-    def __init__(self, db_string):
+    def __init__(self, data, feature_name, output_name):
+        self.feature_name = feature_name
+        self.output_name = output_name
         self.tree = sklearn.tree.DecisionTreeRegressor(
             criterion='mse',
             random_state=42
         )
-        engine = sqlalchemy.create_engine(db_string)
-        data = pd.read_sql('SELECT * FROM person', engine)
         train, test = sklearn.model_selection.train_test_split(
             data, random_state=42
         )
-        self.tree.fit(train[['age']], train['income'])
-        self.accuracy_score = self.tree.score(test[['age']], test['income'])
+        self.tree.fit(train[[self.feature_name]], train[self.output_name])
+        self.test_score = self.tree.score(
+            test[[self.feature_name]], test[self.output_name]
+        )
+        self.train_score = self.tree.score(
+            train[[self.feature_name]], train[self.output_name]
+        )
 
-    def predict(self, age):
+    def predict(self, feature_value):
         """Returns a tuple with (mean, variance)"""
-        node_index = self.tree.apply([[age]])[0]
+        node_index = self.tree.apply([[feature_value]])[0]
         return (
             self.tree.tree_.value[node_index][0][0],
             self.tree.tree_.impurity[node_index]
         )
 
-    def get_accuracy_score(self):
+    def get_score_for_test(self):
         """Returns the R^2 accuracy score of the ML model on the test data."""
-        return self.accuracy_score
+        return self.test_score
+
+    def get_score_for_train(self):
+        """Returns the R^2 accuracy score of the ML model on the training
+        data."""
+        return self.train_score

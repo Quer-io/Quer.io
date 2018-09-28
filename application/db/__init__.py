84828dc1e83fcac6fc000ac2b8e9710949b9cc4e
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-09-28 14:14:16 +0300

    Create move all data access method to db module

--- application/db/__init__.py
+++ application/db/__init__.py
@@ -1,3 +1,4 @@
 from .data_accessor import get_example_row_from_db
 from .data_accessor import get_user_defined_query
 from .data_accessor import get_population_variance_from_db
+from .data_accessor import get_all_data

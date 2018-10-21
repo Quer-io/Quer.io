f1b1fe6a1a8e896844132114fd2f46227e895be7
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-10-21 16:38:12 +0300

    Use Feature in demo_super-advanced

--- demo_scripts/demo_super-advanced.py
+++ demo_scripts/demo_super-advanced.py
@@ -1,12 +1,13 @@
 import sys
 import os.path
 sys.path = [os.path.dirname(__file__) + '/..'] + sys.path
-import quer.io as q
+import querio as q
+from querio.ml import Feature
 
 dB = "postgres://otoihucuckhivv:7b93b9777ab13649dc0af7ef499a699a307c7ffd5ca1733389e1dfb1dac5253a@ec2-54-217-250-0.eu" \
      "-west-1.compute.amazonaws.com:5432/dab0467utv53cp "
 i = q.Interface(dB)
 object = QueryObject("income")
-object.add(cond("age<40") and cond("height>150"))
+object.add(Feature('age') < 30 and Feature('height') > 150)
 result = i.make_query(object)
 print(result)  # > (avg income = 3000; variance income = 2000)

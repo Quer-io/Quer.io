d53be8691966e94c2ee44ec35b2b61241166d898
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-10-21 16:37:10 +0300

    Fix demo_basic

--- demo_scripts/demo_basic.py
+++ demo_scripts/demo_basic.py
@@ -2,15 +2,15 @@ import sys
 import os.path
 sys.path = [os.path.dirname(__file__) + '/..'] + sys.path
 import querio as q
+from querio.ml import Feature
 
-dB = "postgres://otoihucuckhivv:7b93b9777ab13649dc0af7ef499a699a307c7ffd5ca1733389e1dfb1dac5253a@ec2-54-217-250-0.eu" \
-     "-west-1.compute.amazonaws.com:5432/dab0467utv53cp "
+dB = "postgres://otoihucuckhivv:7b93b9777ab13649dc0af7ef499a699a307c7ffd5ca1733389e1dfb1dac5253a@ec2-54-217-250-0.eu-west-1.compute.amazonaws.com:5432/dab0467utv53cp"
 i = q.Interface(dB)
 model = i.train("income", "age")
 model2 = i.train("age", "income")
 
-result = model.predict(Feature('age') == 30)
+result = model.predict([Feature('age') == 30])
 print(str(result))  # > (avg income = 1000; variance income = 4000)
 
-result = model2.predict(Feature('income') == 5000)
+result = model2.predict([Feature('income') == 5000])
 print(str(result))  # > (avg age = 30; variance age = 10)

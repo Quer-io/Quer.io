c6fd663f1550f6e62afdff47b14cd3e326726829
JaykobJ <43149056+JaykobJ@users.noreply.github.com>
2018-10-31 19:17:30 +0200

    Documentation for services

--- querio/service/save_service.py
+++ querio/service/save_service.py
@@ -6,11 +6,24 @@ from .exceptions.querio_file_error import QuerioFileError
 
 
 class SaveService:
+    '''Saves and loads created querio models. User can define the path where these models are stored.
+    Created files have their own naming convention and also unique file name.
+
+    Parameters:
+    path: string
+        File path where the models are stored
+    '''
 
     def __init__(self, path=""):
+        '''Initialize the SaveService'''
         self._src_folder = path
 
     def save_model(self, model):
+        '''Saves the model into a querio file.
+
+        :param model: Model
+            The Model user wants to save
+        '''
         relative_path = self._src_folder + self._generate_name_for_model_attributes(model.output_name, model.get_feature_names())
         file = open(os.path.join(os.getcwd(), relative_path), 'wb+')
 
@@ -19,9 +32,27 @@ class SaveService:
         file.close()
 
     def load_model(self, output_name, feature_names):
+        '''Loads the Model
+
+        :param output_name:
+            The name of the column used to calculate the mean and the variance in
+            queries.
+        :param feature_names:
+            The names of the columns in the data that are used to narrow down the
+            rows.
+        :return:
+            Model defined by the parameters
+        '''
         return self.load_file(self._generate_name_for_model_attributes(output_name, feature_names))
 
     def load_file(self, file_name):
+        '''Returns Model from the spesific file
+
+        :param file_name: String
+            Name of the file where Model is stored
+        :return:
+            Model from the file
+        '''
         relative_path = self._src_folder + file_name
 
         try:
@@ -39,6 +70,7 @@ class SaveService:
         return model
 
     def clear_querio_files(self):
+        '''Delete all querio files from the path folder'''
         path = os.path.join(os.getcwd(), self._src_folder)
         querio_files = self.get_querio_files()
 
@@ -46,9 +78,17 @@ class SaveService:
             os.remove(path + file)
 
     def set_folder (self, folder_path):
+        '''Set new folder path'''
         self._src_folder = folder_path
 
     def model_is_saved(self, model_name):
+        '''Checks if the model is saved in path folder
+
+        :param model_name: String
+            name of the saved model
+        :return:
+            true if model already exist else return false
+        '''
         querio_files = self.get_querio_files()
 
         for querio_file in querio_files:
@@ -71,6 +111,11 @@ class SaveService:
         return name
 
     def get_querio_files(self):
+        '''Return all querio files in path folder
+
+        :return:
+            list of querio files
+        '''
         files = os.listdir(os.path.join(os.getcwd(), self._src_folder))
         querio_files = [file for file in files if self._is_querio_file(file)]
         return querio_files

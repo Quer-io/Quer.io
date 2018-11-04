69468ff73a0d249d23cf44bf8aa61d52f139041a
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-11-04 16:34:15 +0200

    Demo or query in demo_super-advanced

--- demo_scripts/demo_super-advanced.py
+++ demo_scripts/demo_super-advanced.py
@@ -7,7 +7,11 @@ from querio.queryobject import QueryObject
 
 dB = "postgres://otoihucuckhivv:7b93b9777ab13649dc0af7ef499a699a307c7ffd5ca1733389e1dfb1dac5253a@ec2-54-217-250-0.eu-west-1.compute.amazonaws.com:5432/dab0467utv53cp"
 i = q.Interface(dB)
-object = QueryObject("income")
-object.add((Feature('age') < 30) & (Feature('height') > 150))
-result = i.object_query(object)
-print(result)  # > (avg income = 3000; variance income = 2000)
+object1 = QueryObject("income")
+object1.add((Feature('age') < 30) & (Feature('height') > 150))
+object2 = QueryObject("income")
+object2.add((Feature('age') < 30) | (Feature('height') > 150))
+result1 = i.object_query(object1)
+result2 = i.object_query(object2)
+print(result1)  # > (avg income = 3000; variance income = 2000)
+print(result2)  # > (avg income = 3000; variance income = 2000)

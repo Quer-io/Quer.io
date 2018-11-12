770d29215550afca72017b4114ea4cd3d9d72dd8
frestmau <mauri.frestadius@hotmail.fi>
2018-11-12 11:35:30 +0200

    Fixed errors in interface and added documentation to some methods

--- demo_scripts/demo_super-advanced.py
+++ demo_scripts/demo_super-advanced.py
@@ -6,9 +6,10 @@ from querio.ml.expression import Feature
 from querio.queryobject import QueryObject
 
 dB = "postgres://otoihucuckhivv:7b93b9777ab13649dc0af7ef499a699a307c7ffd5ca1733389e1dfb1dac5253a@ec2-54-217-250-0.eu-west-1.compute.amazonaws.com:5432/dab0467utv53cp"
-i = q.Interface(dB)
-object1 = QueryObject("income")
-object1.add((Feature('age') < 30) & (Feature('height') > 150))
+i = q.Interface(dB, "person")
+i.load_models()
+object1 = QueryObject("height")
+object1.add((Feature('age') > 30) & (Feature('income') > 6000))
 object2 = QueryObject("income")
 object2.add((Feature('age') < 30) | (Feature('height') > 150))
 result1 = i.object_query(object1)
--- querio/interface.py
+++ querio/interface.py
@@ -10,22 +10,52 @@ from querio.service.utils import get_frequency_count
 
 class Interface:
     def __init__(self, dbpath,  table_name, savepath=""):
+        """The base class through which the Querio library can be used effectively.
+        It is recomended to use this class for queries, since it handles all necessary functions
+        for the user.
+
+        Parameters:
+        dbpath: string
+            The path to the database in the form postgres://username:password@DatabaseAddress:Port/DatabaseName
+        savepath: string, optional
+            The path that you wish to save the files into. If left blank will be the path from which the program was called.
+
+        """
+
         self.accessor = da.DataAccessor(False, dbpath, table_name)
         self.models = {}
         self.columns = self.accessor.get_table_column_names()
         self.__ss__ = SaveService(savepath)
 
     def train(self, target: str, features: list):
-        print('training new model')
+        """Trains a new model for given data using the features provided.
 
+        Arguments:
+            target: string
+                The target of the model that will be trained.
+            features: list of string
+                The column names of features that will be trained for the model
+         """
         self._validate_columns([target])
         self._validate_columns(features)
 
         feature_names = sorted(features)
-        self.models[target+':'.join(feature_names)] = model.Model(self.accessor.get_all_data(), features, target)
-        return self.models[target+':'.join(feature_names)]
+        self.models[target+':'+''.join(feature_names)] = model.Model(self.accessor.get_all_data(), features, target)
+        return self.models[target+':'+''.join(feature_names)]
 
     def object_query(self, q_object: QueryObject):
+        """Run new query from models using a QueryObject.
+        This will run a query from an existing model,
+        or if no such model is found will train a new one
+        and then query from that.
+
+        Arguments:
+            q_object: QueryObject
+        Returns:
+            A Prediction object that contains the predicted mean and variance of
+            samples matching the given conditions.
+        """
+
         feature_names = []
         for c in q_object.expression:
             if isinstance(c, Cond):
@@ -34,19 +64,24 @@ class Interface:
 
         self._validate_columns(feature_names)
 
-        if q_object.target+':'+q_object.expression not in self.models:
-            self.train(q_object.target, q_object.expression)
+        if q_object.target+':'+''.join(feature_names) not in self.models:
+            self.train(q_object.target, feature_names)
 
-        return self.models[q_object.target+':'+feature_names].query(q_object.expression)
-
+        return self.models[q_object.target+':'+''.join(feature_names)].query(q_object.expression)
 
     def query(self, target: str, conditions: List[Cond]):
         feature_names = generate_list(conditions)
         self._validate_columns(feature_names)
         if target+':'.join(feature_names) not in self.models:
             self.train(target, feature_names)
-
-        return self.models[target+':'.join(feature_names)].query(exp)
+        exp = None
+        if len(conditions) == 1:
+            exp = conditions[0]
+        else:
+            exp = conditions[0] & conditions[1]
+            for i in range(2, len(conditions)):
+                exp = exp & conditions[i]
+        return self.models[target+':'+''.join(feature_names)].query(exp)
 
     def save_models(self):
         for m in self.models:
@@ -96,6 +131,13 @@ class Interface:
 
 
 def generate_list(conditions):
+    """Generates a list without duplicates from given list
+
+    Arguments:
+        conditions: a list of string
+    Returns:
+        """
+
     feature_names = []
     for c in conditions:
         if c.feature not in feature_names:

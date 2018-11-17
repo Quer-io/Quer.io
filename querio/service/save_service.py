6568fa6294f49fbc204b76719ad3db3e1baa5d07
Dennis A <28817715+Dforssi@users.noreply.github.com>
2018-11-17 09:16:41 +0200

    Q-9.7, services are PEP8 compliant

--- querio/service/save_service.py
+++ querio/service/save_service.py
@@ -39,15 +39,17 @@ class SaveService:
         """Loads specific Model
 
          :param output_name: string
-            The name of the column used to calculate the mean and the variance in
-            queries.
+            The name of the column used to calculate the mean and the
+            variance in queries.
         :param feature_names: list of strings
-            The names of the columns in the data that are used to narrow down the
-            rows.
+            The names of the columns in the data that are used
+            to narrow down the rows.
         :return:
             Model defined by the parameters
         """
-        return self.load_file(self._generate_name_for_model_attributes(output_name, feature_names))
+        return self.load_file(self._generate_name_for_model_attributes(
+                                                                output_name,
+                                                                feature_names))
 
     def load_file(self, file_name):
         """Returns Model from the specific file
@@ -62,14 +64,17 @@ class SaveService:
         try:
             file = open(os.path.join(os.getcwd(), relative_path), 'rb')
         except FileNotFoundError as e:
-            raise QuerioFileError("No model found with following name: " + file_name, e)
+            raise QuerioFileError(
+                "No model found with following name: " +
+                file_name, e)
 
         try:
             model = pickle.load(file)
         except pickle.PickleError as e:
             raise QuerioFileError(
                         file_name +
-                        " could not be loaded as a model. Please train a new model",
+                        """ could not be loaded as a model. 
+                        Please train a new model""",
                         e)
         finally:
             file.close()
@@ -130,4 +135,3 @@ class SaveService:
         files = os.listdir(os.path.join(os.getcwd(), self._src_folder))
         querio_files = [file for file in files if self._is_querio_file(file)]
         return querio_files
-

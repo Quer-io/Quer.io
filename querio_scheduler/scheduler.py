f03ac20d00c7a43b84c34016f47e6c3a112bb469
xbexbex <xbexbex@github.com>
2018-12-04 23:10:04 +0000

    scheduler training now possible without db string or table name

--- querio_scheduler/scheduler.py
+++ querio_scheduler/scheduler.py
@@ -2,23 +2,41 @@ import querio as q
 import schedule
 import time
 import os
+import logging
 
 
 class Scheduler:
 
-    def __init__(self, db_path: str,  table_name: str, save_path: str = "", time_string: str = "02:00"):
-        self.i = q.Interface(db_path, table_name, save_path)
+    def __init__(self, save_path: str = "", time_string: str = "02:00"):
         self.save_path = save_path
         self.time_string = time_string
+        self.logger = logging.getLogger("QuerioScheduler")
 
     def set_time(self, time_string: str):
         self.time_string = time_string
 
+    def set_path(self, save_path: str):
+        self.save_path = save_path
+
     def retrain(self):
-        self.i.retrain_saved_models()
-        self.i.save_models()
-        self.i.clear_models()
-        print("Successfully retrained saved models")
+        ss = q.service.save_service.SaveService(self.save_path)
+        files = ss.get_querio_files()
+        for f in files:
+            try:
+                m = ss.load_file(f)
+                db_path = m.db_path
+                table_name = m.table_name
+                i = q.Interface(db_path, table_name, self.save_path)
+                i.load_models()
+                i.retrain_models()
+                i.save_models()
+                self.logger.info("""Successfully retrained saved model '{}'"""
+                                 .format(f))
+            except q.interface.QuerioColumnError:
+                self.logger.error("""Encountered an error when retraining model '{}'."""
+                                  .format(f))
+            continue
+        print("Retraining completed")
 
     def get_complete_path(self):
         return os.path.join(os.getcwd(), self.save_path)
@@ -35,4 +53,3 @@ class Scheduler:
         except KeyboardInterrupt:
             print("Stopped")
             pass
-

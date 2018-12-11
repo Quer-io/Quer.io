2eb2cf10796d0ee92172426808d6c628d54e352d
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-12-11 14:45:35 +0200

    Change documentation wording

--- querio/interface.py
+++ querio/interface.py
@@ -24,8 +24,8 @@ class Interface:
         The path that you wish to save the files into.
         If left blank will be the path from which the program was called.
     model_params: dict, optional
-        A keyword arguments dict used to pass arguments to the decision tree
-        model. See Scikit Learn documentation on decision tree regressors for
+        A keyword arguments dict used to pass arguments to trained models.
+        See Scikit Learn documentation on decision tree regressors for
         accepted parameters and theirs function.
 
     """

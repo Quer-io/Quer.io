73adcbbe2912f2099a4ff7afe7ee81bc8e606b5b
Kim Toivonen <kim.toivonen@outlook.com>
2018-12-12 16:03:51 +0200

    Add testing of normalized database to travis

---
+++ tests/test_travis_normalized_db.py
@@ -0,0 +1,32 @@
+import os
+import unittest
+from querio import Interface
+from querio.ml.expression.feature import Feature
+from querio.queryobject import QueryObject
+
+is_travis = 'TRAVIS' in os.environ
+
+class TravisNormalizedDatabaseTest(unittest.TestCase):
+
+
+    def setUp(self):
+        self.db_uri = 'postgres://postgres:@localhost:5432/normaldb'
+
+    def test_make_query_for_querio_view(self):
+        if not is_travis:
+            return
+        
+        i = q.Interface(dB, 'querio_view')
+
+        object1 = QueryObject('height')
+        object1.add((Feature('profession_name') == 'programmer'))
+        object1.add(Feature('stars') > 20)
+
+        result1 = i.object_query(object1)
+
+        self.assertTrue(result1 is not None)
+
+
+
+
+

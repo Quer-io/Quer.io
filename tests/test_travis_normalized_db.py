cf82b2d4b64f85916254381f957b73b557900880
Kim Toivonen <kim.toivonen@outlook.com>
2018-12-12 16:27:27 +0200

    Trial to fix the mock_constructor problem in tests

--- tests/test_travis_normalized_db.py
+++ tests/test_travis_normalized_db.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import querio as q
+from querio.interface import Interface
 from querio.ml.expression.feature import Feature
 from querio.queryobject import QueryObject
 
@@ -10,13 +11,14 @@ class TravisNormalizedDatabaseTest(unittest.TestCase):
 
 
     def setUp(self):
+        Interface.__init__ = q.Interface.__init__
         self.db_uri = 'postgres://postgres:@localhost:5432/normaldb'
 
     def test_make_query_for_querio_view(self):
         if not is_travis:
             return
         
-        i = q.Interface(self.db_uri, 'querio_view')
+        i = Interface(self.db_uri, 'querio_view')
 
         object1 = QueryObject('height')
         object1.add((Feature('profession_name') == 'programmer'))
